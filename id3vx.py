667dd368ce9c574dc2e5ed04c3888e34b7be3ded
Horst Schneider <horst.schneider@posteo.de>
2019-09-26 01:38:22 +0200

    Check in some dirty code

--- id3vx.py
+++ id3vx.py
@@ -5,6 +5,43 @@ from struct import unpack
 import sys
 
 
+class Frame:
+    Header = namedtuple('Header', 'identifier, size, flags')
+
+    class Flags(IntFlag):
+        """
+        http://id3.org/id3v2.3.0#Frame_header_flags
+        """
+        TagAlterPreservation = 1 << 14
+        FileAlterPreservation = 1 << 13
+        ReadOnly = 1 << 12
+        Compression = 1 << 7
+        Encryption = 1 << 6
+        GroupingIdentity = 1 << 5
+
+    def __init__(self, header, payload):
+        self.__header = header
+        self.__payload = payload
+
+    def header(self):
+        return self.__header
+
+    def payload(self):
+        return self.__payload.decode("iso-8859-1")
+
+    @staticmethod
+    def read_from(mp3):
+        f_head = Frame.Header(*unpack('>4slh', mp3.read(10)))
+
+        if not f_head.size:
+            return None
+
+        return Frame(f_head, mp3.read(f_head.size))
+
+    def __str__(self):
+        return f'{self.header().identifier}: {self.payload()}'
+
+
 class TagHeader:
     """
     Represents the 10-byte ID3v2.x tag header that must be present
@@ -19,27 +56,46 @@ class TagHeader:
         Experimental = 1 << 4
 
     ID3_IDENTIFIER = "ID3"
-    Fields = namedtuple('TagHeader', 'identifier, major, minor, flags, size')
+    SIZE = 10
+    Fields = namedtuple('TagHeader', 'identifier, major, minor, flags, tag_size')
 
     def __init__(self, path_to_mp3):
         with open(path_to_mp3, 'rb') as mp3:
-            fields = unpack('>3sBBBl', mp3.read(10))
-            self.__header = TagHeader.Fields(*fields)
+            # TODO: separate tag and tag header
+            fields = unpack('>3sBBBl', mp3.read(TagHeader.SIZE))
 
-        if self._identifier() != TagHeader.ID3_IDENTIFIER:
-            raise Exception("No ID3v2.x Tag Header found.")
+            self.__fields = TagHeader.Fields(*fields)
+
+            if self._identifier() != TagHeader.ID3_IDENTIFIER:
+                raise Exception("No ID3v2.x Tag Header found.")
+
+            self.__frames = []
+
+            while True:
+                frame = Frame.read_from(mp3)
+
+                if not frame:
+                    break
+
+                self.__frames.append(frame)
 
     def _flags(self):
-        return TagHeader.Flags(self.__header.flags)
+        return TagHeader.Flags(self.__fields.flags)
 
     def _identifier(self):
-        return self.__header.identifier.decode("utf-8")
+        return self.__fields.identifier.decode("utf-8")
+
+    def frames(self):
+        return self.__frames
 
     def __repr__(self):
-        return repr(self.__header)
+        return repr(self.__fields)
 
 
 if __name__ == "__main__":
     header = TagHeader(sys.argv[1])
 
     print(header)
+
+    for frame in header.frames():
+        print(frame)
