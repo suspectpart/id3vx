7d55417352cbd2ed8f8ae747d246a36e67066c0e
Horst Schneider <horst.schneider@posteo.de>
2019-09-26 09:54:04 +0200

    Refactor Tag and TagHeader

--- id3vx.py
+++ id3vx.py
@@ -4,13 +4,24 @@ from collections import namedtuple
 from struct import unpack
 import sys
 
+ENCODING = "iso-8859-1"
+
 
 class Frame:
+    """
+    Represents a single 10-byte ID3v2.3 frame.
+
+    Refer to `ID3v2.3 specification
+    <http://id3.org/id3v2.3.0#ID3v2_frame_overview>`_
+    """
     Header = namedtuple('Header', 'identifier, size, flags')
 
     class Flags(IntFlag):
         """
-        http://id3.org/id3v2.3.0#Frame_header_flags
+        Represents 2-byte flags included in a frame header.
+
+         Refer to `ID3v2.3 specification
+        <http://id3.org/id3v2.3.0#Frame_header_flags>`_
         """
         TagAlterPreservation = 1 << 14
         FileAlterPreservation = 1 << 13
@@ -27,7 +38,7 @@ class Frame:
         return self.__header
 
     def payload(self):
-        return self.__payload.decode("iso-8859-1")
+        return self.__payload.decode(ENCODING)
 
     @staticmethod
     def read_from(mp3):
@@ -43,32 +54,61 @@ class Frame:
 
 
 class TagHeader:
-    """
-    Represents the 10-byte ID3v2.x tag header that must be present
-    at the very start of a tagged mp3 file.
+    """ID3v2.3 tag header.
 
-    See `ID3v2 tag header specification <http://id3.org/id3v2.3.0#ID3v2_header>`
+    Represents the 10-byte ID3v2.3 tag header that must be present
+    at the very start of a tagged mp3 file, prefixed with the magic
+    file signature "TAG".
+
+    See `ID3v2 tag header specification
+    <http://id3.org/id3v2.3.0#ID3v2_header>`_
     """
+
+    ID3_IDENTIFIER = "ID3"
+    SIZE = 10
+    Fields = namedtuple('TagHeader',
+                        'identifier, major, minor, flags, tag_size')
+
+    def __init__(self, fields):
+        self.__fields = fields
+
+        if self.identifier() != TagHeader.ID3_IDENTIFIER:
+            raise ValueError("No ID3v2.x Tag Header found.")
+
+    def __len__(self):
+        """Size of the header. It's 10."""
+
+        return TagHeader.SIZE
+
+    def identifier(self):
+        return self.__fields.identifier.decode(ENCODING)
+
+    @staticmethod
+    def read_from(mp3):
+        block = unpack('>3sBBBl', mp3.read(TagHeader.SIZE))
+
+        return TagHeader(TagHeader.Fields(*block))
+
+    def __repr__(self):
+        return repr(self.__fields)
+
+
+class Tag:
 
     class Flags(IntFlag):
+        """
+        Represents the 1-byte header flags.
+
+        See `ID3v2.3 tag header specification
+        http://id3.org/id3v2.3.0#ID3v2_header`_
+        """
         Sync = 1 << 6
         Extended = 1 << 5
         Experimental = 1 << 4
 
-    ID3_IDENTIFIER = "ID3"
-    SIZE = 10
-    Fields = namedtuple('TagHeader', 'identifier, major, minor, flags, tag_size')
-
     def __init__(self, path_to_mp3):
         with open(path_to_mp3, 'rb') as mp3:
-            # TODO: separate tag and tag header
-            fields = unpack('>3sBBBl', mp3.read(TagHeader.SIZE))
-
-            self.__fields = TagHeader.Fields(*fields)
-
-            if self._identifier() != TagHeader.ID3_IDENTIFIER:
-                raise Exception("No ID3v2.x Tag Header found.")
-
+            self.__header = TagHeader.read_from(mp3)
             self.__frames = []
 
             while True:
@@ -79,23 +119,20 @@ class TagHeader:
 
                 self.__frames.append(frame)
 
-    def _flags(self):
-        return TagHeader.Flags(self.__fields.flags)
-
-    def _identifier(self):
-        return self.__fields.identifier.decode("utf-8")
+    def header(self):
+        return self.__header
 
     def frames(self):
         return self.__frames
 
     def __repr__(self):
-        return repr(self.__fields)
+        return repr(self.header())
 
 
 if __name__ == "__main__":
-    header = TagHeader(sys.argv[1])
+    tag = Tag(sys.argv[1])
 
-    print(header)
+    print(tag)
 
-    for frame in header.frames():
+    for frame in tag.frames():
         print(frame)
